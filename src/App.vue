07781e3bac01c4e1ad06826c59d498cbb143946c
mozartc <mozartc@126.com>
2020-05-09 13:49:42 +0800

    新增全部项目

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted(){
+      window.addEventListener('unload', this.saveState);
+  },
+  provide () {    //父组件中通过provide来提供变量，在子组件中通过inject来注入变量。                                             
+            return {
+                reload: this.reload                                              
+            }
+        },
+        data() {
+            return{
+                isRouterAlive: true                    //控制视图是否显示的变量
+            }
+        },
+        methods: {
+            reload () {
+                this.isRouterAlive = false;            //先关闭，
+                this.$nextTick(function () {
+                    this.isRouterAlive = true;         //再打开
+                }) 
+            },
+            saveState() {
+                sessionStorage.setItem('state', JSON.stringify(this.$store.state))
+            }
+        },
+}
+</script>
